9b8f6d78f0b72faf6bf555370091a7561346e134
Amru <root@WelcomeFuture>
2023-09-18 07:06:10 +0700

    UI : create button component

--- src/components/SharedUI/Button.jsx
+++ src/components/SharedUI/Button.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
-export const Button = () => {
-  return (
-    <div>Button</div>
-  )
+export const Button = ({variant, children}) => {
+    if ( variant === 'primary') 
+      return <button className='bg-black text-white p=4 rounded-lg'>{children}</button>;
+      return <button className='bg-zinc-400 text-white p=4 rounded-lg'>{children}</button>
 }
