2e1c4eb5077410ad9bafa929728ca473bf202eb2
Amru <root@WelcomeFuture>
2023-09-22 18:38:30 +0700

    (cleanup input classname at components/Register.jsx, redundant since we've import imput from nextUI),(Change class to className at Input tag in LandingPage.Header.jsx),(Change and use to  Import Image at SharedUI/Card.jsx),(in globals.css put .card at @layer components not in @layer base)

--- src/components/LandingPage/components/LandingPage.Header.jsx
+++ src/components/LandingPage/components/LandingPage.Header.jsx
@@ -23,7 +23,7 @@ export const Header = () => {
         viewBox="0 0 24 24" 
         stroke-width="1.5" 
         stroke="currentColor" 
-        class="w-6 h-6">
+        className="w-6 h-6">
             <path 
             stroke-linecap="round" 
             stroke-linejoin="round" 
